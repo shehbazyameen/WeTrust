7b27b13d0c8aab9eeee3313c7c5d747de2c7dc36
shehbazyameen <50536514+shehbazyameen@users.noreply.github.com>
2022-10-13 11:08:23 -0700

    Colors added

--- src/config/Colors.js
+++ src/config/Colors.js
@@ -1,3 +1,9 @@
 export const colors = {
   inputFieldBackColor: '#f6f6f6',
+  primaryBlue: '#191C4C',
+  primaryBlueOpacity50: '#191C4C50',
+  screenColor: '#F5F5F5',
+  white: '#FFFFFF',
+  btnColor1: '#AC872E',
+  textColor1: '#00AEEF',
 };
